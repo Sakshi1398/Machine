d4b51082fed0346486910d25ca3398f059cbb6ed
Sakshi Kumari <sakshikumari3617@gmail.com>
2022-06-24 13:29:37 +0530

    First commit...

---
+++ Day1/Matplotlib.py
@@ -0,0 +1,6 @@
+
+import matplotlib.pyplot as plt
+y_value = [100,120,140,160,180,200,240,260,280,300,400,500]
+x_value = [10,20,30,40,50,60,70,80,90,12,34,67,]
+plt.plot(x_value,y_value)
+plt.show()
---
+++ Day1/Pandas.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df=pd.read_csv(r"C:\Users\SIDHANTH SUMAN\Downloads\Mypandas\my_data\survey_results_public.csv")
+print(df)
